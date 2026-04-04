1396b8993d9f796e7badd80ecfdfcbbe47fa74c3
Matthew <matthewjwright07@gmail.com>
2026-04-04 13:54:35 -0500

    site wide updates

---
+++ static/main.js
@@ -0,0 +1,12 @@
+const mobile_hamburger = document.querySelector(".mobile-hamburger")
+const mobile_close = document.querySelector(".mobile-close")
+const mobile_menu = document.querySelector(".mobile-menu")
+
+mobile_hamburger.addEventListener("click", ()=>{
+    mobile_menu.style.display = "flex"
+})
+mobile_close.addEventListener("click", ()=>{
+    mobile_menu.style.display = "none"
+})
+
+
