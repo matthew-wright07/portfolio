efce3697d84d565424739710cdabc367a95454b9
Matthew <matthewjwright07@gmail.com>
2026-04-29 17:31:28 -0500

    menu updates

--- static/script.js
+++ static/script.js
@@ -16,13 +16,34 @@ const menu = document.querySelector(".menu");
 var open = false;
 const menu_content = document.querySelector(".menu-content")
 
-menu.addEventListener("click",()=>{
-    open=!open;
-    if (open===true){
-        menu.src="/static/images/close.svg";
-        menu_content.style.display = "flex"
-    }else{
-        menu.src="/static/images/menu.svg"
-        menu_content.style.display = "none"
+const openPath = `
+<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+`;
+
+const closedPath = `
+<path fill-rule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
+`;
+
+menu.addEventListener("click", () => {
+    open = !open;
+
+    if (open) {
+        menu.innerHTML = openPath;
+
+        menu.setAttribute("viewBox", "0 0 24 24");
+        menu.setAttribute("fill", "none");
+        menu.setAttribute("stroke", "currentColor");
+        menu.setAttribute("stroke-width", "1.5");
+
+        menu_content.style.display = "flex";
+    } else {
+        menu.innerHTML = closedPath;
+
+        menu.setAttribute("viewBox", "0 0 24 24");
+        menu.setAttribute("fill", "currentColor");
+        menu.removeAttribute("stroke");
+        menu.removeAttribute("stroke-width");
+
+        menu_content.style.display = "none";
     }
-})
+});
